c6f5192f9f2b6b033b7df9f3e7573e9387adbf6d
Mister_Anderson <neo69429@ya.ru>
2020-03-28 04:18:01 +0300

    Changes

--- task3/script.py
+++ task3/script.py
@@ -1,18 +1,19 @@
 from pathlib import Path
 import sys
+import subprocess
 from subprocess import run
 
 
 def run_cmd(*args):
-    ret = run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out = ret.stdout
-    err = ret.stderr
+    ret = run(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = ret.stdout.decode('utf-8')
+    err = ret.stderr.decode('utf-8')
 
     return f'out:\n{out}\nerr:\n{err}\n'
 
 
 with open(sys.argv[1], 'w') as f:
-    print(run_cmd('uname', '-a'), file=f)
-    print(run_cmd('ip', 'addr', 'show'), file=f)
-    print(run_cmd('echo', '$USER'), file=f)
+    print(run_cmd('uname -a'), file=f)
+    print(run_cmd('ip addr show'), file=f)
+    print(run_cmd('echo $USER'), file=f)
 
