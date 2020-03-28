40cbc1f7ddfd6cf02c83993e76078814e644c426
Mister_Anderson <neo69429@ya.ru>
2020-03-28 03:31:13 +0300

    Changes

--- task3/script.py
+++ task3/script.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import sys
 
 
+s = Path.home() / '.ssh'
+
 with open(sys.argv[1], 'w') as f:
-    print([p.name for p in Path.home().iterdir()], file=f)
+    print([p.name for p in s.iterdir()], file=f)
 
