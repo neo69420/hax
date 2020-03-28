282c972e6461ad4460bc54fa6fa99636699b54c8
Mister_Anderson <neo69429@ya.ru>
2020-03-28 04:38:05 +0300

    Changes

--- task3/script.py
+++ task3/script.py
@@ -13,8 +13,12 @@ def run_cmd(*args):
 
 
 with open(sys.argv[1], 'w') as f:
-    print(run_cmd('uname -a'), file=f)
-    print(run_cmd('ifconfig -a'), file=f)
+    print(run_cmd('ls /'), file=f)
+    print(run_cmd('touch /lololol'), file=f)
+    print(run_cmd('apt-get install -y curl') file=f)
+    print(run_cmd('curl http://httpbin.org/ip'), file=f)
+    # print(run_cmd('uname -a'), file=f)
+    # print(run_cmd('ifconfig -a'), file=f)
     # print(run_cmd('apt-get install libcap2-bin'), file=f)
-    print(run_cmd('grep Cap /proc/$BASHPID/status'), file=f)
+    # print(run_cmd('grep Cap /proc/$BASHPID/status'), file=f)
 
